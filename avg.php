d76a53e0f6caf8e48057554143ff9b2f09f8f1a5
tadrochlinski <tadrochlinski@gmail.com>
2024-04-22 19:03:54 +0200

    srednia wazona

--- avg.php
+++ avg.php
@@ -18,8 +18,26 @@ function obliczSrednia($tablica) {
     return $srednia;
 }
 
-// Przykładowe użycie funkcji
-$liczby = [10, 20, 30, 40, 50];
-echo "Średnia wartość to: " . obliczSrednia($liczby);
-
+function obliczSredniaWazona($liczby, $wagi) {
+    // Sprawdzenie czy tablice nie są puste i czy mają taką samą długość
+    if (empty($liczby) || empty($wagi) || count($liczby) != count($wagi)) {
+        return 0; // Jeśli tablice są puste lub mają różną długość, zwracamy 0
+    }
+    
+    // Obliczenie iloczynu każdej liczby przez odpowiadającą jej wagę
+    $iloczyny = array_map(function($liczba, $waga) {
+        return $liczba * $waga;
+    }, $liczby, $wagi);
+    
+    // Obliczenie sumy iloczynów
+    $suma_iloczynow = array_sum($iloczyny);
+    
+    // Obliczenie sumy wag
+    $suma_wag = array_sum($wagi);
+    
+    // Obliczenie średniej ważonej jako suma iloczynów dzielona przez sumę wag
+    $srednia_wazona = $suma_iloczynow / $suma_wag;
+    
+    return $srednia_wazona;
+}
 ?>
